410f06d310ba9883da05a89c37a3757d64fb1d83
kevin <kevinzy17@gmail.com>
2022-01-12 19:59:08 -0800

    added for and while loop exercises

--- js_exercises/script.js
+++ js_exercises/script.js
@@ -96,3 +96,32 @@ const lastLetter = (string) => {
 
 c(lastLetter("COWfaskdifsidfdsikX"));
 c(lastLetter("abcd"));
+
+for (let i = 2; i <= 10; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  } else {
+    continue;
+  }
+}
+
+// for (let i = 0; i < 3; i++) {
+//   alert (`number ${i}`);
+// }
+
+// let i = 0
+// while (i < 3) {
+//   alert(`number ${i}`)
+//   i++
+// }
+
+let number = 0;
+answer = prompt("Enter a number: ");
+
+while (number !== 100) {
+  if (answer >= 100 || "") {
+    break;
+  } else {
+    answer = prompt("Enter a number: ");
+  }
+}
