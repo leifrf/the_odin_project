8698e6e217cd1ee77a0554728bd31beff7a2a3fe
kevin <kevinzy17@gmail.com>
2022-01-12 22:23:15 -0800

    new version

--- js_exercises/rock-paper-scissors/script.js
+++ js_exercises/rock-paper-scissors/script.js
@@ -1,105 +1,116 @@
-// create a function that will randomly return 1 of the following: "Rock", "Paper", "Scissors". this function will be the computer's play
+let playerScore = 0;
+let computerScore = 0;
+let roundWinner = "";
+let round = 0;
 
-function computerSelection() {
-  // Use math.random to get a value between 0 and 2 (note: end is not inclusive)
-  // Floor it to get rid of decimals so numbers stay between 0-2
-  let computerMove = Math.floor(Math.random() * 3);
-
-  if (computerMove === 0) {
-    // 0 = Rock
-    return "Rock";
-  } else if (computerMove === 1) {
-    // 1 = Paper
-    return "Paper";
-  } else {
-    // 2 = Scissors
-    return "Scissors";
+function getRandomChoice() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return "ROCK";
+    case 1:
+      return "PAPER";
+    case 2:
+      return "SCISSORS";
   }
 }
 
-// Make prompt case insensitive
-function capitalize(string) {
-  return string.slice(0, 1).toUpperCase() + string.slice(1).toLowerCase();
-}
+function playRPS(playerSelection, computerSelection = getRandomChoice()) {
+  if (playerSelection === computerSelection) {
+    roundWinner = "tie";
+    round++;
+  }
 
-// write a function that plays a single round of Rock, Paper Scissors (RPS). The function takes 2 parameters (playerSelection, computerSelection). Then it returns a string that declares the winner of the round so: "You Lose! Paper beats Rock!" note: make sure to return values, not console.log(). But you can use console.log() to test your code
+  if (
+    (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
+    (playerSelection === "SCISSORS" && computerSelection === "PAPER") ||
+    (playerSelection === "PAPER" && computerSelection === "ROCK")
+  ) {
+    playerScore++;
+    round++;
+    roundWinner = "player";
+  }
 
-function playRPS(playerSelection, computerPlay = computerSelection()) {
-  let lose = `${computerPlay} beats ${playerSelection}. You lose!`;
-  let win = `${playerSelection} beats ${computerPlay}. You win!`;
+  if (
+    (playerSelection === "ROCK" && computerSelection === "PAPER") ||
+    (playerSelection === "SCISSORS" && computerSelection === "ROCK") ||
+    (playerSelection === "PAPER" && computerSelection === "SCISSORS")
+  ) {
+    computerScore++;
+    round++;
+    roundWinner = "computer";
+  }
 
-  if (playerSelection === "Rock" && computerPlay === "Paper") {
-    return lose;
-  } else if (playerSelection === "Rock" && computerPlay === "Scissors") {
-    return win;
-  } else if (playerSelection === "Scissors" && computerPlay === "Rock") {
-    return lose;
-  } else if (playerSelection === "Scissors" && computerPlay === "Paper") {
-    return win;
-  } else if (playerSelection === "Paper" && computerPlay === "Scissors") {
-    return lose;
-  } else if (playerSelection === "Paper" && computerPlay === "Rock") {
-    return win;
-  } else if (playerSelection === computerPlay) {
-    return "It's a tie!";
+  console.log(`Round ${round}`);
+  console.log("Player Score: ", playerScore);
+  console.log("Computer Score: ", computerScore);
+
+  if (roundWinner === "computer") {
+    console.log("The computer won this round.");
+  } else if (roundWinner === "player") {
+    console.log("You won this round!");
   } else {
-    return `${playerSelection} is not valid. Please try again with one of these options: [Rock, Paper, Scissors]`;
+    console.log("It was a tie!");
   }
 }
 
-const rockBtnElement = document.querySelector("#rock");
-const paperBtnElement = document.querySelector("#paper");
-const scissorsBtnElement = document.querySelector("#scissors");
+/////////////////////////////////////////////////////
 
-function game() {
-  const winningScore = 5;
-  let computerWins = 0;
-  let playerWins = 0;
-  let rounds = 1;
-  let playerMove = "";
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
 
-  rockBtnElement.addEventListener("click", () => {
-    if (playRPS("Rock")) {
-      playerMove === "Rock";
+rockBtn.addEventListener("click", () => {
+  if (playerScore !== 5 && computerScore !== 5) {
+    playRPS("ROCK");
+
+    if (playerScore === 5) {
+      console.log("You win!");
     }
-  });
 
-  paperBtnElement.addEventListener("click", () => {
-    if (playRPS("Paper")) {
-      playerMove === "Paper";
+    if (computerScore === 5) {
+      console.log("You lost!");
     }
-  });
-  scissorsBtnElement.addEventListener("click", () => {
-    if (playRPS("Scissors")) {
-      playerMove === "Scissors";
-    }
-  });
-
-  const computerMove = computerSelection();
-  const roundResult = playRPS(playerMove, computerMove);
-
-  console.log(`Current round: ${rounds}`);
-
-  if (roundResult.search("You win!") > -1) {
-    playerWins++;
-    console.log(roundResult);
-  } else if (roundResult.search("You lose!") > -1) {
-    computerWins++;
-    console.log(roundResult);
-  } else {
-    console.log(roundResult);
   }
-  rounds++;
+});
+paperBtn.addEventListener("click", () => {
+  if (playerScore !== 5 && computerScore !== 5) {
+    playRPS("PAPER");
 
-  const finalResult = `
-    Number of Rounds: ${rounds - 1}
-    Scores:
-    You: ${playerWins}
-    Computer: ${computerWins}`;
+    if (playerScore === 5) {
+      console.log("You win!");
+    }
 
-  if (playerWins === winningScore) {
-    console.log(`You win! ${finalResult}`);
-  } else {
-    console.log(`You lost...${finalResult}`);
+    if (computerScore === 5) {
+      console.log("You lost!");
+    }
   }
+});
+scissorsBtn.addEventListener("click", () => {
+  if (playerScore !== 5 && computerScore !== 5) {
+    playRPS("SCISSORS");
+
+    if (playerScore === 5) {
+      console.log("You win!");
+    }
+
+    if (computerScore === 5) {
+      console.log("You lost!");
+    }
+  }
+});
+
+function unitTest() {
+  // Player = Rock
+  playRPS("ROCK", "ROCK");
+  playRPS("ROCK", "SCISSOR");
+  playRPS("ROCK", "PAPER");
+  // Player = PAPER
+  playRPS("PAPER", "ROCK");
+  playRPS("PAPER", "SCISSOR");
+  playRPS("PAPER", "PAPER");
+  // Player = SCISSOR
+  playRPS("SCISSOR", "ROCK");
+  playRPS("SCISSOR", "SCISSOR");
+  playRPS("SCISSOR", "PAPER");
 }
